5c33cae069136ad8f3ed76678c0b6a1fa7dea4c4
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-10-08 20:42:17 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,8 +3,60 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+import subprocess
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_extract():
+    """tests extract()"""
+    result = subprocess.run(
+        ["python", "main.py", "extract"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Extracting data..." in result.stdout
+
+
+def test_transform_load():
+    """tests transfrom_load"""
+    result = subprocess.run(
+        ["python", "main.py", "transform_load"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Transforming data..." in result.stdout
+
+
+def test_general_query():
+    """tests general_query"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "general_query",
+            """SELECT team AS Team, COUNT(*) AS TotalMatchesPlayed
+            FROM (SELECT tm_gms AS team FROM default.matchesdb_one
+                
+                SELECT * FROM default.matchesdb_one
+                join 
+                select * from default.wwc_matches_2_db
+                on default.matchesdb_one.tm_gms = default.wwc_matches_2_db.team1
+
+                ) AS table3
+            GROUP BY Team1 
+            ORDER BY data DESC;""",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+if __name__ == "__main__":
+    test_extract()
+    test_transform_load()
+    test_general_query()
