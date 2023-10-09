d06eecfe4d5bbe6c91973abbad8f754330c881ba
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-10-08 20:20:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-"""
-ETL-Query script
-"""
-
+"""handles cli commands"""
+import sys
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import query
+from mylib.query import (
+    general_query,
+)
 
-# Extract
-print("Extracting data...")
-extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+def handle_arguments(args):
+    """add action based on inital call"""
+    parser = argparse.ArgumentParser(description="Complex SQL query script")
+    parser.add_argument(
+        "action",
+        choices=[
+            "extract",
+            "transform_load",
+            "general_query",
+        ],
+    )
+    args = parser.parse_args(args[:1])
+    print(args.action)
 
-# Query
-print("Querying data...")
-query()
+    if args.action == "general_query":
+        parser.add_argument("query")
+
+    # parse again with ever
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    """handles all the cli commands"""
+    args = handle_arguments(sys.argv[1:])
+
+    if args.action == "extract":
+        print("Extracting data...")
+        extract()
+    elif args.action == "transform_load":
+        print("Transforming data...")
+        load()
+    elif args.action == "general_query":
+        general_query(args.query)
+
+    else:
+        print(f"Unknown action: {args.action}")
+
+
+if __name__ == "__main__":
+    main()
